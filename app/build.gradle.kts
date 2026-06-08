a80e9e3d88971d9091c67f4f6daffe7f26ab6162
Adhil <adhil.mhdk28@gmail.com>
2026-06-08 21:46:57 +0530

    feat: add Material3 and AndroidX design resources for application release bundle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "sols.sync"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
