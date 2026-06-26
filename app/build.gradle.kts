c71d19f5b3778dfe9a5c9d591853b6c50e7af1d6
Adhil <adhil.mhdk28@gmail.com>
2026-06-26 10:52:10 +0530

    chore(repo): modify .idea and 1057 more files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "sols.sync"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.2"
+        versionCode = 5
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
