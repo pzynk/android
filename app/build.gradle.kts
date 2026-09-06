85b59e628fb36de17b7511912af97def28615f3a
Adhil <adhil.mhdk28@gmail.com>
2026-09-06 15:24:14 +0530

    ms

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "sols.sync"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.0.4"
+        versionCode = 7
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
